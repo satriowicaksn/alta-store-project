57ec0c0ce9935babc87fcca546e8ac95f967c4d8
satriowicaksono87 <satriowicaksono076@gmail.com>
2021-08-22 00:49:01 +0700

    inisialisasi middleware

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"alta-store-project/config"
 	"alta-store-project/routes"
+	"fmt"
 )
 
 func main() {
 	config.InitDB()
 	e := routes.New()
 	e.Logger.Fatal(e.Start(":8000"))
+	fmt.Println("test midd")
 }

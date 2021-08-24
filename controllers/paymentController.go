d54b2ed521cb5dce650a2ceff296cd5729e487fd
satriowicaksono87 <satriowicaksono076@gmail.com>
2021-08-24 22:09:05 +0700

    menerapkan has many pada struct Checkout dan Checkout Item

--- controllers/paymentController.go
+++ controllers/paymentController.go
@@ -2,7 +2,7 @@ package controllers
 
 import (
 	"alta-store-project/lib/database"
-	// "alta-store-project/middlewares"
+	"alta-store-project/middlewares"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -16,5 +16,51 @@ func GetPaymentMethodController(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"status": "success",
 		"data":   paymentMethod,
+	})
+}
+
+func PostPaymentController(c echo.Context) error {
+	userId := middlewares.ExtractTokenUserId(c)
+	payloadData := make(map[string]string)
+	payloadData["payment_id"] = c.FormValue("payment_id")
+	payloadData["amount"] = c.FormValue("amount")
+	payloadData["payment_method"] = c.FormValue("payment_method")
+	payment, err := database.PostPayment(userId, payloadData)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	if payment == false {
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"status":  "success",
+			"message": "Your payment was not processed because the requested data is incomplete",
+		})
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"status": "success",
+		"data":   payment,
+	})
+}
+
+func GetPendingPaymentController(c echo.Context) error {
+	userId := middlewares.ExtractTokenUserId(c)
+	payment, err := database.GetPendingPayment(userId)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"status": "success",
+		"data":   payment,
+	})
+}
+
+func GetPaymentHistoryController(c echo.Context) error {
+	userId := middlewares.ExtractTokenUserId(c)
+	payment, err := database.GetPaymentHistory(userId)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"status": "success",
+		"data":   payment,
 	})
 }
--- models/checkout.go
+++ models/checkout.go
@@ -1,7 +1,9 @@
 package models
 
 type Checkout struct {
-	Product    string `json:"product"`
-	Item_total int    `json:"item_total"`
-	Amount     int    `json:"amount"`
+	Checkout_id   int         `json:"checkout_id"`
+	Product       string      `json:"product"`
+	Item_total    int         `json:"item_total"`
+	Amount        int         `json:"amount"`
+	Checkout_item []Cart_item `json:"items" gorm:"foreignKey:Cart_id;references:Checkout_id"`
 }

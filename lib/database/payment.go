1aca8bd08babc60070ff7e838c071ea2478c4c1a
satriowicaksono87 <satriowicaksono076@gmail.com>
2021-08-25 13:49:21 +0700

    menambahkan validasi untuk cek payment data yang dikirim user dan memperbaiki responsenya jika invalid

--- lib/database/payment.go
+++ lib/database/payment.go
@@ -22,8 +22,8 @@ func GetPaymentMethod() (interface{}, error) {
 }
 
 func GetPaymentHistory(userId int) (interface{}, error) {
-	var payment []models.Payment
-	if err := config.DB.Where("user_id = ? AND payment_status = 1", userId).Find(&payment).Error; err != nil {
+	var payment []models.Payment_history
+	if err := config.DB.Raw("SELECT payment_id, payment_method_name, amount, payed_at FROM payments LEFT JOIN payment_methods ON payments.payment_method = payment_methods.payment_method_id WHERE user_id = ? AND payment_status = 1", userId).Scan(&payment).Error; err != nil {
 		return nil, err
 	}
 	return payment, nil
@@ -34,20 +34,32 @@ func GetPendingPayment(userId int) (interface{}, error) {
 	if err := config.DB.Where("user_id = ? AND payment_status = 0", userId).Find(&payment).Error; err != nil {
 		return nil, err
 	}
+	// for _, payments := range payment {
+	// 	paymentItem := []models.Payment_item{}
+	// 	config.DB.Where("payment_id = ?", payments.Payment_id).Find(&paymentItem)
+	// 	payments.Payment_item = paymentItem
+	// }
 	return payment, nil
 }
 
 func PostPayment(userId int, payloadData map[string]string) (interface{}, error) {
 	payment := models.Payment{}
-	payment_id, _ := strconv.Atoi(payloadData["payment_id"])
 	payment_method, _ := strconv.Atoi(payloadData["payment_method"])
 	amount, _ := strconv.Atoi(payloadData["amount"])
-	check := config.DB.Where("payment_id = ? AND amount = ? AND user_id = ?", payment_id, amount, userId).Find(&payment)
+	check := config.DB.Where("payment_id = ? AND user_id = ? AND amount = ? AND payment_status = 0", payloadData["payment_id"], userId, amount).Find(&payment)
+	isValid := CheckPaymentMethod(payment_method)
+	if !isValid {
+		// Jika payment method tidak valid
+		return "Payment method invalid", nil
+	}
 	if check.Error != nil {
 		return nil, check.Error
-	}
-	if check.RowsAffected == 0 {
-		return false, nil
+	} else if check.RowsAffected == 0 {
+		checkPayStatus := config.DB.Where("payment_id = ? AND user_id = ? AND amount = ? AND payment_status = 1", payloadData["payment_id"], userId, amount).Find(&payment)
+		if checkPayStatus.RowsAffected > 0 {
+			return "This bill has been paid", nil
+		}
+		return "The bill you want to pay was not found", nil
 	}
 	paymentUpdate := models.Payment{
 		Payment_method: payment_method,
@@ -56,4 +68,12 @@ func PostPayment(userId int, payloadData map[string]string) (interface{}, error)
 	}
 	config.DB.Where("payment_id = ?", payloadData["payment_id"]).Updates(&paymentUpdate)
 	return paymentUpdate, nil
+}
+func CheckPaymentMethod(id int) bool {
+	paymentMethod := models.Payment_method{}
+	query := config.DB.Where("payment_method_id = ? ", id).Find(&paymentMethod)
+	if query.RowsAffected == 0 {
+		return false
+	}
+	return true
 }

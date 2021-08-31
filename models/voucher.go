6e8c4968070e39e73b71cb199e4c8012709b57f4
satriowicaksono87 <satriowicaksono076@gmail.com>
2021-08-31 11:10:30 +0700

    menambahkan struct untuk table voucher

---
+++ models/voucher.go
@@ -0,0 +1,28 @@
+package models
+
+import "time"
+
+type Voucher struct {
+	Voucher_id          int       `json:"voucher_id" gorm:"primaryKey"`
+	Voucher_code        string    `json:"voucher_code"`
+	Description         string    `json:"description"`
+	Voucher_type        int       `json:"voucher_type"`
+	Voucher_disc        int       `json:"disc"`
+	Minimum_transaction int       `json:"minimum_transaction"`
+	Created_at          time.Time `json:"created_at"`
+	Updated_at          time.Time `json:"updated_at"`
+}
+
+type User_voucher struct {
+	User_id    int `json:"user_id"`
+	Voucher_id int `json:"voucher_id"`
+	Status     int `json:"status"`
+}
+
+type Payment_voucher struct {
+	Payment_id   string `json:"payment_id"`
+	Voucher_code string `json:"voucher_code"`
+	Total_bill   int    `json:"total_bill"`
+	Disc         int    `json:"discount"`
+	Final_bill   int    `json:"final_bill"`
+}
